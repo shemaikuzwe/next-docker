78e04954b3210a15cfac6141142c53bb9821c112
shemaikuzwe <eshemaikuzwe@gmail.com>
2025-03-28 11:35:05 +0200

    adding seed

---
+++ prisma/seed.ts
@@ -0,0 +1,30 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  console.log("Seeding users...");
+
+  const users = [
+    { name: "John Doe", email: "john@example.com", password: "1234" },
+    { name: "Jane Smith", email: "jane@example.com", password: "1234" },
+    { name: "Sam Wilson", email: "sam@example.com", password: "1234" },
+  ];
+
+  for (const user of users) {
+    await prisma.user.create({
+      data: user,
+    });
+  }
+
+  console.log("Seeding completed!");
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
